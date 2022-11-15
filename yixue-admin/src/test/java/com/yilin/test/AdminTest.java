7db9c4d4169d443d8ae65778e4731b8772ebc400
yilin7987 <1178990518@qq.com>
2022-11-15 21:23:51 +0800

    test

---
+++ yixue-admin/src/test/java/com/yilin/test/AdminTest.java
@@ -0,0 +1,11 @@
+package com.yilin.test;
+
+import com.yilin.yixueblog.controller.AdminController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AdminTest {
+
+}
