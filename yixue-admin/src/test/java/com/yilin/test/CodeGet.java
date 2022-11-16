5d4cf0059ef0e104fd38d589088eb7410b4cf011
yilin7987 <1178990518@qq.com>
2022-11-16 22:29:03 +0800

    代码生成器（mybatis-plus）

---
+++ yixue-admin/src/test/java/com/yilin/test/CodeGet.java
@@ -0,0 +1,69 @@
+package com.yilin.test;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+
+public class CodeGet {
+    public static void main(String[] args) {
+        // 1、创建代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 2、全局配置
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        //gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setOutputDir("F:\\IDEA\\yixue-blog\\yixue-admin"+"/src/main/java");
+
+        gc.setServiceName("%sService");	//去掉Service接口的首字母I
+        gc.setAuthor("yilin");  //作者名（就是类注释那个名字）
+        gc.setOpen(false);
+        mpg.setGlobalConfig(gc);
+
+        // 3、数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/yixue_blog_db");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        // 4、包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.yilin");
+        pc.setModuleName("yixueblog"); //模块名
+        pc.setController("controller");
+        pc.setEntity("entity");
+        pc.setService("service");
+        pc.setMapper("mapper");
+        mpg.setPackageInfo(pc);
+
+        // 5、策略配置
+        StrategyConfig strategy = new StrategyConfig();
+
+        strategy.setInclude("tb_blog");   //那个表
+
+        strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
+
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);//数据库表字段映射到实体的命名策略
+        strategy.setEntityLombokModel(true); // lombok 模型 @Accessors(chain = true) setter链式操作
+
+        strategy.setRestControllerStyle(true); //restful api风格控制器
+        strategy.setControllerMappingHyphenStyle(true); //url中驼峰转连字符
+
+        mpg.setStrategy(strategy);
+
+        // 6、执行
+        mpg.execute();
+    }
+}
