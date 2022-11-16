7d27ea6719f595957969e0883500713584ecbf6b
yilin7987 <1178990518@qq.com>
2022-11-16 21:15:36 +0800

    基类

---
+++ yixue-model/src/main/java/com/yilin/yixueblog/vo/BaseVO.java
@@ -0,0 +1,17 @@
+package com.yilin.yixueblog.vo;
+
+import lombok.Data;
+
+/**
+ * BaseVO   view object 表现层 基类对象
+ */
+@Data
+public class BaseVO<T> extends PageInfo<T> {
+
+    /**
+     * 唯一UID
+     */
+    private String uid;
+
+    private Integer status;
+}
